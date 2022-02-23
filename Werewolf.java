b4579618c7bc17cc1d6a9405c787f9fb99c81b2c
saths008 <63187492+saths008@users.noreply.github.com>
2022-02-23 15:16:48 +0000

    Fixing up a few comments and their terminology

--- Werewolf.java
+++ Werewolf.java
@@ -2,41 +2,43 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * A simple model of a fox.
- * Werewolves age, move, eat rabbits, and die.
+ * A simple model of a werewolf.
+ * Werewolves age, move, eat unicorn/jackalopes, and die.
  * 
  * @author David J. Barnes and Michael Kölling
  * @version 2016.02.29 (2)
  */
 public class Werewolf extends Animal
 {
-    // Characteristics shared by all foxes (class variables).
+    // Characteristics shared by all werewolves (class variables).
     
-    // The age at which a fox can start to breed.
+    // The age at which a werewolf can start to breed.
     private static final int BREEDING_AGE = 15;
-    // The age to which a fox can live.
+    // The age to which a werewolf can live.
     private static final int MAX_AGE = 150;
-    // The likelihood of a fox breeding.
+    // The likelihood of a werewolf breeding.
     private static final double BREEDING_PROBABILITY = 0.08;
     // The maximum number of births.
     private static final int MAX_LITTER_SIZE = 2;
-    // The food value of a single rabbit. In effect, this is the
-    // number of steps a fox can go before it has to eat again.
-    private static final int RABBIT_FOOD_VALUE = 9;
+    // The food value of a single unicorn/jackalope. In effect, this is the
+    // number of steps a werewolf can go before it has to eat again.
+    private static final int JACKALOPE_FOOD_VALUE = 9;
+
+    private static final int UNICORN_FOOD_VALUE = 10;
     // A shared random number generator to control breeding.
     private static final Random rand = Randomizer.getRandom();
     
     // Individual characteristics (instance fields).
-    // The fox's age.
+    // The werewolf's age.
     private int age;
-    // The fox's food level, which is increased by eating rabbits.
+    // The werewolf's food level, which is increased by eating unicorns/jackalopes.
     private int health;
 
     /**
-     * Create a fox. A fox can be created as a new born (age zero
+     * Create a werewolf. A werewolf can be created as a new born (age zero
      * and not hungry) or with a random age and food level.
      * 
-     * @param randomAge If true, the fox will have random age and hunger level.
+     * @param randomAge If true, the werewolf will have random age and hunger level.
      * @param field The field currently occupied.
      * @param location The location within the field.
      */
@@ -45,20 +47,20 @@ public class Werewolf extends Animal
         super(field, location);
         if(randomAge) {
             age = rand.nextInt(MAX_AGE);
-            health = rand.nextInt(RABBIT_FOOD_VALUE);
+            health = rand.nextInt(JACKALOPE_FOOD_VALUE);
         }
         else {
             age = 0;
-            health = RABBIT_FOOD_VALUE;
+            health = JACKALOPE_FOOD_VALUE;
         }
     }
     
     /**
-     * This is what the fox does most of the time: it hunts for
-     * rabbits. In the process, it might breed, die of hunger,
+     * This is what the werewolf does most of the time: it hunts for
+     * unicorn/jackalope. In the process, it might breed, die of hunger,
      * or die of old age.
 //     * @param field The field currently occupied.
-     * @param newWerewolves A list to return newly born foxes.
+     * @param newWerewolves A list to return newly born werewolves.
      */
     public void act(List<Animal> newWerewolves)
     {
@@ -84,7 +86,7 @@ public class Werewolf extends Animal
     }
 
     /**
-     * Increase the age. This could result in the fox's death.
+     * Increase the age. This could result in the werewolf's death.
      */
     private void incrementAge()
     {
@@ -95,7 +97,7 @@ public class Werewolf extends Animal
     }
     
     /**
-     * Make this fox more hungry. This could result in the fox's death.
+     * Make this werewolf more hungry. This could result in the werewolf's death.
      */
     private void incrementHunger()
     {
@@ -106,8 +108,8 @@ public class Werewolf extends Animal
     }
     
     /**
-     * Look for rabbits adjacent to the current location.
-     * Only the first live rabbit is eaten.
+     * Look for unicorns/jackalopes adjacent to the current location.
+     * Only the first live unicorn/jackalope is eaten.
      * @return Where food was found, or null if it wasn't.
      */
     private Location findFood()
@@ -116,23 +118,28 @@ public class Werewolf extends Animal
         List<Location> adjacent = field.adjacentLocations(getLocation());
         for (Location where : adjacent) {
             Object animal = field.getObjectAt(where);
-            if (animal instanceof Jackalope jackalope && jackalope.isAlive()) {
+            if ((animal instanceof Jackalope jackalope && jackalope.isAlive()))  {
                     jackalope.setDead();
-                    health = RABBIT_FOOD_VALUE;
+                    health += JACKALOPE_FOOD_VALUE;
                     return where;
+            }
+            else if ((animal instanceof Unicorn unicorn && unicorn.isAlive())) {
+                unicorn.setDead();
+                health += UNICORN_FOOD_VALUE;
+                return where;
             }
         }
         return null;
     }
     
     /**
-     * Check whether or not this fox is to give birth at this step.
+     * Check whether or not this werewolf is to give birth at this step.
      * New births will be made into free adjacent locations.
-     * @param newWerewolves A list to return newly born foxes.
+     * @param newWerewolves A list to return newly born werewolves.
      */
     private void giveBirth(List<Animal> newWerewolves)
     {
-        // New foxes are born into adjacent locations.
+        // New werewolves are born into adjacent locations.
         // Get a list of adjacent free locations.
         Field field = getField();
         List<Location> free = field.getFreeAdjacentLocations(getLocation());
@@ -159,10 +166,11 @@ public class Werewolf extends Animal
     }
 
     /**
-     * A fox can breed if it has reached the breeding age.
+     * A werewolf can breed if it has reached the breeding age.
      */
     private boolean canBreed()
     {
         return age >= BREEDING_AGE;
     }
+
 }
